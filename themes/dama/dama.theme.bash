c75c7366a283c2f1eb188859cc8f0603373f285e
Zoom.Quiet <zoomquiet+git@gmail.com>
2015-10-22 23:53:18 +0800

    collection all local cfg.

--- themes/dama/dama.theme.bash
+++ themes/dama/dama.theme.bash
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
+# ↑     →     ↓     ↖     ↗     ↘     ↙ 
+# ∫ § Γ Λ  Ξ  ζ  ξ ρ σ  
+# ♪ ♩ ♫ ♬ ¶ ‖♭ ♯ § ∮ 
+# ི ྀ ོ ༄ ࿓ ༆ ༅ ࿔ ༺ ༻ ༇
+# ༼ ༽ ࿐
+# ࿕ ࿖ ྊ ྾ ༗ ༘ ༉ ࿈ ༒ ༴ ࿑ ࿄ ༑ ࿅
+# ࿉ ࿋ ༔ ࿊ ࿌ ༓ ༶ ༊ ࿂ ࿃ 
+# ༎ ༐ ། ༏ 
 
-THEME_PROMPT_SEPARATOR=""
+THEME_PROMPT_SEPARATOR="${grey}༽"
 
 SHELL_SSH_CHAR=" "
 SHELL_THEME_PROMPT_COLOR=32
 SHELL_SSH_THEME_PROMPT_COLOR=208
 
-VIRTUALENV_CHAR="ⓔ "
-VIRTUALENV_THEME_PROMPT_COLOR=35
+VIRTUALENV_CHAR="${yellow}༼"
+VIRTUALENV_THEME_PROMPT_COLOR=37
+#VIRTUALENV_THEME_PROMPT_COLOR="${cyan}"
 
 SCM_NONE_CHAR=""
-SCM_GIT_CHAR="  "
+SCM_GIT_CHAR="⎇ "
+#SCM_GIT_CHAR="  "
 
-SCM_THEME_PROMPT_CLEAN=""
-SCM_THEME_PROMPT_DIRTY=""
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" |"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
 
 SCM_THEME_PROMPT_COLOR=238
 SCM_THEME_PROMPT_CLEAN_COLOR=231
@@ -54,7 +71,9 @@ function powerline_virtualenv_prompt {
     fi
 
     if [[ -n "$environ" ]]; then
-        VIRTUALENV_PROMPT="${THEME_PROMPT_SEPARATOR}$(set_rgb_color ${VIRTUALENV_THEME_PROMPT_COLOR} -) ${VIRTUALENV_CHAR}$environ ${normal}"
+        #VIRTUALENV_PROMPT="${THEME_PROMPT_SEPARATOR}$(set_rgb_color ${VIRTUALENV_THEME_PROMPT_COLOR} -) ${VIRTUALENV_CHAR}$environ ${normal}"
+        #VIRTUALENV_PROMPT="$(set_rgb_color ${VIRTUALENV_THEME_PROMPT_COLOR} -)${VIRTUALENV_CHAR}$environ${normal}${THEME_PROMPT_SEPARATOR}"
+        VIRTUALENV_PROMPT="${VIRTUALENV_CHAR}${cyan}$environ${normal}${THEME_PROMPT_SEPARATOR}"
     else
         VIRTUALENV_PROMPT=""
     fi
@@ -74,21 +93,24 @@ function powerline_scm_prompt {
             SCM_PROMPT="$(set_rgb_color ${SCM_THEME_PROMPT_CLEAN_COLOR} -)"
         fi
         if [[ "${SCM_GIT_CHAR}" == "${SCM_CHAR}" ]]; then
-            SCM_PROMPT+=" ${SCM_CHAR}${SCM_BRANCH}${SCM_STATE} "
+            SCM_PROMPT+="${grey}${SCM_CHAR}${purple}${SCM_BRANCH}${SCM_STATE}"
         fi
-        SCM_PROMPT="${THEME_PROMPT_SEPARATOR}${SCM_PROMPT}${normal}"
+        #SCM_PROMPT="${THEME_PROMPT_SEPARATOR}${SCM_PROMPT}${normal}"
+        SCM_PROMPT="${SCM_PROMPT}${normal}${THEME_PROMPT_SEPARATOR}"
     else
         SCM_PROMPT=""
     fi
 }
 
 function powerline_cwd_prompt {
-    CWD_PROMPT="${THEME_PROMPT_SEPARATOR} \w "
+    #CWD_PROMPT="${THEME_PROMPT_SEPARATOR}\w"
+    CWD_PROMPT="${green}\w${normal}"
 }
 
 function powerline_last_status_prompt {
     if [[ "$1" -eq 0 ]]; then
         LAST_STATUS_PROMPT="${THEME_PROMPT_SEPARATOR}"
+        #LAST_STATUS_PROMPT="${yellow}༆ ${normal} "
     else
         LAST_STATUS_PROMPT="${THEME_PROMPT_SEPARATOR}$(set_rgb_color ${LAST_STATUS_THEME_PROMPT_COLOR} -) ${LAST_STATUS} ${normal}${THEME_PROMPT_SEPARATOR}"
     fi
@@ -103,8 +125,12 @@ function powerline_prompt_command() {
     powerline_cwd_prompt
     powerline_last_status_prompt LAST_STATUS
 
-    PS1="${SHELL_PROMPT}${VIRTUALENV_PROMPT}${SCM_PROMPT}${CWD_PROMPT}${LAST_STATUS_PROMPT} "
+    #PS1="${SHELL_PROMPT}${VIRTUALENV_PROMPT}${SCM_PROMPT}${CWD_PROMPT}${LAST_STATUS_PROMPT} "
+    #PS1="${VIRTUALENV_PROMPT}${SCM_PROMPT}${CWD_PROMPT}${LAST_STATUS_PROMPT} "
+    PS1="${VIRTUALENV_PROMPT}${SCM_PROMPT}${CWD_PROMPT}${LAST_STATUS_PROMPT}\n${yellow}༆ ${normal} "
 }
 
 PROMPT_COMMAND=powerline_prompt_command
 
+
+#export PS1='${ORANGE}༄ ${D} ${GREEN}\W${D}\n${PINK}\$${D} '
